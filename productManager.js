d5362cd6ec1ed0704321cfe90e4d120fe5252e68
Bruselas24 <bruiva2003@hotmail.com>
2023-02-08 03:41:59 -0300

    FEATURE Asincronismo y Errores
    Se añade el uso de asincronismo en los metodos del Product Manager ademas del respectivo control de errores

--- productManager.js
+++ productManager.js
@@ -1,51 +1,54 @@
-import {existsSync, readFileSync, writeFileSync} from "fs";
+import {existsSync, promises, readFileSync, writeFileSync} from "fs";
 
 class ProductManager{
     constructor() {
         this.path = './productManager.txt'
-        this.id = 0
 
         if(!(existsSync(this.path))){
             writeFileSync(this.path,JSON.stringify([]))
         }
-
         this.products = JSON.parse(readFileSync(this.path,'utf-8'))
+
+        this.id = [...this.products].pop().id
     }
 
-    addProduct(newProduct){
+    async addProduct(newProduct){
 
         this.products.forEach(prod => {
             if(prod.code === newProduct.code){
                 throw Error('Repeated product code')
             }
         })
-        newProduct.id = this.id++
+
+        newProduct.id = ++this.id
         this.products.push(newProduct)
-       writeFileSync(this.path,JSON.stringify(this.products))
+        await promises.writeFile(this.path,JSON.stringify(this.products))
     }
 
-    getProducts(){
-        return JSON.parse(readFileSync(this.path,'utf-8'))
+    async getProducts(){
+        return JSON.parse(await promises.readFile(this.path,'utf-8'))
     }
 
     getProductById(pid){
-        return this.products[pid]
+        return this.products.find((product => product.id === pid))
     }
 
-    updateProduct(pid,field,newValue){
+    async updateProduct(pid,field,newValue){
 
         if(field === 'id') throw Error('the ID can´t be modified')
 
-        this.products[pid][field] = newValue
-        writeFileSync(this.path,JSON.stringify(this.products))
+        this.getProductById(pid)[field] = newValue
+        await promises.writeFile(this.path,JSON.stringify(this.products))
     }
 
-    deleteProduct(pid){
+    async deleteProduct(pid){
 
-        if(!this.products[pid]) throw Error('Product does not exist')
+        const productIndex = this.products.indexOf(this.getProductById(pid))
 
-        this.products.splice(pid,1)
-        writeFileSync(this.path, JSON.stringify(this.products))
+        if(!(productIndex > -1)) throw Error('Product does not exist')
+
+        this.products.splice(productIndex,1)
+        await promises.writeFile(this.path, JSON.stringify(this.products))
     }
 }
 
